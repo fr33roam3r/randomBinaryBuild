42429601cc749c5889114958747d05bdbc545845
Zac Fuqua <Agaped@pm.me>
2020-07-30 16:46:20 -0400

    functionalize

--- rBB.js
+++ rBB.js
@@ -1,2 +1,14 @@
-var milliseconds = new Date().getMilliseconds().toString().split('')
-var binary = Number(milliseconds[2]) % 2
+function randomBinary () {
+    var milliseconds = new Date().getMilliseconds().toString().split('')
+    return Number(milliseconds[2]) % 2
+}
+        
+function randomBinaryBuild (min, max) {
+    do {
+        var binaryString = ''
+        for (var i = 0; i < max.toString(2).length; i++) {
+            binaryString += randomBinary()
+        }            
+    } while ( (min > parseInt(binaryString)) || (max < parseInt(binaryString)) )
+    return parseInt(binaryString)
+}
